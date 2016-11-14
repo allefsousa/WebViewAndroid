61cf1347726d610e496ace064d28690c314f8897
allef <allefsousa_1@hotmail.com>
2016-11-13 23:27:44 -0200

    projeto webView

--- app/src/main/java/com/example/allef/myapplication/MainActivity.java
+++ app/src/main/java/com/example/allef/myapplication/MainActivity.java
@@ -9,18 +9,18 @@ import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button nav ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url ="http://localhost:85/site";
-        WebView wv=(WebView) findViewById(R.id.webView1);
+        String url ="http://google.com"; // declarando url que sera acessada
+        WebView wv=(WebView) findViewById(R.id.webView1);  // fazendo referencia ao layout
         wv.getSettings().setJavaScriptEnabled(true);
         wv.getSettings().setPluginState(WebSettings.PluginState.ON);
         wv.getSettings().setAllowFileAccess(true);
-        wv.loadUrl(url);
+        wv.loadUrl(url); // carregando a url
 
 
     }
